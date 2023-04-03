88b0b13f350cf7f0561fe45947e05a85ac75be60
josephbajor <josephbajor@gmail.com>
2023-04-02 23:48:26 -0400

    self-attention checkpoint

--- hparams.py
+++ hparams.py
@@ -7,11 +7,13 @@ class Hparams:
         "/home/jbajor/Dev/CMU/Research/fairseq/examples/language_model/wikitext-103"
     )
     tokenized_dir: str = "data"
-    vocab_size: int = 3000
+    vocab_size: int = 1000
 
     num_workers = 4
     batch_size = 128
     epochs = 25
 
-    block_size = 64
-    embed_size = 32
+    max_span = 64
+    embed_size = 128
+    att_block_size = 64
+    num_heads = 3
--- src/model.py
+++ src/model.py
@@ -30,6 +30,41 @@ class SimpleBigramModel(nn.Module):
         return x
 
 
+class FullAttention(nn.Module):
+    def __init__(self, max_seq_len, embed_size, block_size, autoregressive=False):
+        super().__init__()
+
+        self.max_seq_len = max_seq_len
+
+        self.q = nn.Linear(embed_size, block_size, bias=False)
+        self.k = nn.Linear(embed_size, block_size, bias=False)
+        self.v = nn.Linear(embed_size, block_size, bias=False)
+
+        self.autoregressive = autoregressive
+        if autoregressive:
+            self.mask = torch.tril(torch.ones(max_seq_len, max_seq_len))
+
+    def forward(self, x):
+        q = self.q(x)
+        k = self.k(x)
+        v = self.v(x)
+
+        attention_values = q @ k.transpose(-2, -1)
+        if self.autoregressive:
+            attention_values = attention_values.masked_fill(
+                self.mask == 0, float("-inf")
+            )
+
+        attention_values = attention_values / torch.sqrt(self.block_size)
+        attention_values = F.softmax(
+            attention_values / torch.sqrt(self.block_size), dim=-1
+        )
+
+        z = attention_values @ v
+
+        return z
+
+
 class AttentionLM(nn.Module):
     def __init__(self, hparams: Hparams, vocab_size) -> None:
         super().__init__()
@@ -39,5 +74,32 @@ class AttentionLM(nn.Module):
 
         # Network Components
         self.embed = nn.Embedding(vocab_size, hparams.embed_size)
-        self.embed_pos = nn.Embedding(hparams.block_size, hparams.embed_size)
+        self.embed_pos = nn.Embedding(hparams.max_span, hparams.embed_size)
         self.l1 = nn.Linear(hparams.embed_size, vocab_size)
+
+    def forward(self, x):
+        emb = self.embed(x)
+        pos_emb = self.embed_pos(torch.arange(x.shape[-1], device="cuda"))
+
+        x = emb + pos_emb
+        x = F.gelu(x)
+
+        logits = self.l1(x)
+
+        return logits
+
+    def generate_batch(self, x, seq_len, deterministic=False):
+        if len(x.shape) == 2:
+            x = x.unsqueeze(dim=0)
+
+        for seq in range(seq_len):
+            logits = self(x)[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            if deterministic:
+                next_idx = torch.argmax(dim=1)
+            else:
+                next_idx = torch.multinomial(input=probs, dim=1, num_samples=1)
+
+            x = torch.cat((x, next_idx), dim=1)
+
+        return x
