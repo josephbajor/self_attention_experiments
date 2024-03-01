fix masking, use wordpeice decoding

src/model/LM.py
@@ -43,8 +43,9 @@ class AttentionLM(nn.Module):
         self.use_positional_embedding = hparams.use_positional_embedding
         self.max_span = hparams.max_span
 
-        att_func_type = hparams.att_func_type
-        emb_func = hparams.emb_func
+        self.att_func_type = hparams.att_func_type
+        self.emb_func = hparams.emb_func
+        self.embed_size = hparams.embed_size
 
         # Network Components
         self.embed = nn.Embedding(vocab_size, hparams.embed_size)
@@ -56,9 +57,13 @@ class AttentionLM(nn.Module):
         self.attention = nn.ModuleList(
             [
                 AttentionBlock(
-                    attention_func=ATT_FUNC_MAP[att_func_type],
+                    attention_func=ATT_FUNC_MAP[self.att_func_type],
                     hparams=hparams,
-                    emb_func=ENC_FUNC_MAP[emb_func] if emb_func is not None else None,
+                    emb_func=(
+                        ENC_FUNC_MAP[self.emb_func]
+                        if self.emb_func is not None
+                        else None
+                    ),
                 )
                 for _ in range(hparams.att_layers)
             ]
@@ -80,6 +85,8 @@ class AttentionLM(nn.Module):
         if self.use_positional_embedding:
             pos_emb = self.embed_pos(torch.arange(x.shape[-1], device=x.device))
             x = emb + pos_emb
+        else:  # no positional embedding
+            x = emb
         x = self.pre_att_dropout(x)
         for layer in self.attention:
             x = layer(x)

src/model/attention.py
@@ -44,7 +44,7 @@ class FullAttention(nn.Module):
         self,
         hparams: Hparams,
         emb_func: Optional[nn.Module] = None,
-        masked: bool = False,
+        masked: bool = True,
     ) -> None:
         super().__init__()
         if hparams.embed_size % hparams.num_heads != 0:
@@ -102,7 +102,6 @@ class FullAttention(nn.Module):
                 q,
                 k,
                 v,
-                attn_mask=self.mask,
                 is_causal=self.masked,
                 dropout_p=self.dropout if self.training else 0,
             )
@@ -111,7 +110,7 @@ class FullAttention(nn.Module):
             attention_values = q @ k.transpose(-2, -1)
             if self.masked:
                 attention_values = attention_values.masked_fill(
-                    self.mask == 0, float("-inf")
+                    self.mask[:, :, :T, :T] == 0, float("-inf")
                 )
 
             attention_values = F.softmax(

src/preprocessing.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 from src import logger
 
-from tokenizers import Tokenizer
+from tokenizers import Tokenizer, decoders
 from tokenizers.models import BPE, WordPiece
 from tokenizers.trainers import BpeTrainer, WordPieceTrainer
 from tokenizers.pre_tokenizers import Whitespace
@@ -78,6 +78,9 @@ def preprocess_wikitext_wordpeice(path, vocab_size):
         data_out[fname] = np.concatenate(
             [np.array(t.ids, dtype=np.int32) for t in tokens]
         )
+
+    # add wordpeice decoder to tokenizer
+    tokenizer.decoder = decoders.WordPiece()
 
     return data_out, tokenizer
 
@@ -172,7 +175,7 @@ if __name__ == "__main__":
         logger.info("Saving tokenizer...")
         tokenizer.save(f"{hparams.tokenized_dir}/tokenizer.json")
 
-    if args.dataset == "shakespeare":
+    elif args.dataset == "shakespeare":
         hparams = Hparams()
         preprocess_shakespeare_char(hparams.tokenized_dir, hparams.train_split)
         logger.info("Shakespeare data preprocessed and saved to disk")

src/train.py
@@ -100,12 +100,13 @@ def train(device="cuda"):
                 x = x.to(device)
                 y = y.to(device)
 
-                if hparams.type == "nanogpt":
-                    logits, loss = model(x, y)
+                with torch.cuda.amp.autocast(enabled=hparams.half_precision):
+                    if hparams.type == "nanogpt":
+                        logits, loss = model(x, y)
 
-                elif hparams.type == "gpt":
-                    logits = model(x)
-                    loss = loss_fn(logits.transpose(-1, -2), y)
+                    elif hparams.type == "gpt":
+                        logits = model(x)
+                        loss = loss_fn(logits.transpose(-1, -2), y)
 
                 scaler.scale(loss).backward()
                 # clip gradients
@@ -168,7 +169,7 @@ def train(device="cuda"):
                             test,
                             model,
                             tokenizer,
-                            out_len=20,
+                            out_len=80,
                             determenistic=False,
                             device=device,
                             mode=hparams.dataset,
@@ -178,7 +179,7 @@ def train(device="cuda"):
                     # put model back into training mode
                     model.train()
 
-                if step % 10000 == 0 and step > 0:
+                if step % 3000 == 0 and step > 0:
                     # save training checkpoint
                     logger.info(f"Saving checkpoint at step {step} to {save_pth}")
                     torch.save(model.state_dict(), f"{save_pth}/model.pth")